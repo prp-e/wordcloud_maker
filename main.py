ea21b2021c8df6c1aae2c441204ab2fd552c43d4
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-20 22:04:49 +0430

    added my list of stop words, not deployed

--- main.py
+++ main.py
@@ -18,6 +18,9 @@ stop_words = [word.rstrip('\n') for word in stop_words.readlines()]
 stop_words_additional = open("stopwords_fa_additional.txt")
 stop_words_additional = [word.rstrip('\n') for word in stop_words_additional.readlines()]
 
+stop_words_me = open("stopwords_fa_me.txt")
+stop_words_me = [word.rstrip('\n') for word in stop_words_me.readlines()]
+
 stop_words = list(set(stop_words + stop_words_additional)) # Removing any duplicates wich can happen
 
 text_file = open(sys.argv[1])
