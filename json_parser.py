ee62d9d4b34091455934999fdf5aa9a24795c32f
Muhammadreza Haghiri <haghiri75@gmail.com>
2021-04-20 21:33:47 +0430

    it's done

--- json_parser.py
+++ json_parser.py
@@ -8,4 +8,17 @@ jsons_parsed = []
 
 with open(json_file) as j:
     for line in j.readlines():
-        jsons.append(line)
+        jsons.append(line.rstrip('\n'))
+
+for obj in jsons:
+    jsons_parsed.append(json.loads(obj))
+
+tweets = []
+for obj in jsons_parsed:
+    tweets.append(obj['tweet'])
+
+with open('tweets.txt', 'w') as output:
+    for tweet in tweets:
+        output.write(tweet + '\n')
+
+    
